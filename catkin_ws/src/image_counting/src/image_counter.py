0aac0f8594bd9f7a6bf621364c65d25cc69fb0f4
hulloanson <hulloanson@gmail.com>
2023-08-03 21:59:24 -0400

    converted image_counter to oop

--- catkin_ws/src/image_counting/src/image_counter.py
+++ catkin_ws/src/image_counting/src/image_counter.py
@@ -6,17 +6,6 @@ import rospy
 
 import os 
 
-file_path_publisher = None
-
-def get_cwd():
-  return os.path.dirname(os.path.realpath(__file__))
-
-counter = 0
-
-def reset(*args, **kwargs):
-    global counter
-    counter = 0
-  
 def is_prime(num):
     if num > 1:
         for i in range(2, int(num/2)+1):
@@ -26,28 +15,38 @@ def is_prime(num):
             return True
     else:
         return False
+class ImageCounter:
+  
+  def __init__(self):
+    self.file_path_publisher = None
+    self.counter = 0
 
-def on_cam_img_msg(msg):
-    global counter
-    counter += 1
-    if is_prime(counter):
-      print(counter, 'is prime')
-      path = f'{os.getcwd()}/{counter}.jpg'
-      with open(path, 'wb') as f:
-        f.write(msg.data)
-        f.flush()
-      
-      if file_path_publisher is not None:
-        file_path_publisher.publish(path)
-      
+  def reset(self, *args, **kwargs):
+      self.counter = 0
+    
+
+  def on_cam_img_msg(self, msg):
+      self.counter += 1
+      if is_prime(self.counter):
+        path = f'{os.getcwd()}/{self.counter}.jpg'
+        with open(path, 'wb') as f:
+          f.write(msg.data)
+          f.flush()
+        
+        if self.file_path_publisher is not None:
+          self.file_path_publisher.publish(path)
+        
+  def run(self):
+    file_path_publisher = self.file_path_publisher
+    rospy.init_node('image_counter', anonymous=True)
+    file_path_publisher = rospy.Publisher('/img_path', String, queue_size=10)
+    image_subscriber = rospy.Subscriber('/cam_img', CompressedImage, self.on_cam_img_msg)
+    reset_subscriber = rospy.Subscriber('/reset', Empty, self.reset)
+    rospy.spin()
 
 def main():
-  global file_path_publisher 
-  rospy.init_node('image_counter', anonymous=True)
-  file_path_publisher = rospy.Publisher('/img_path', String, queue_size=10)
-  image_subscriber = rospy.Subscriber('/cam_img', CompressedImage, on_cam_img_msg)
-  reset_subscriber = rospy.Subscriber('/reset', Empty, reset)
-  rospy.spin()
-  
+  counter = ImageCounter()
+  counter.run()
+    
 if __name__ == "__main__":
     main()
